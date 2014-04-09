ac45a4daff610821905330bf19ab0a56b82e08cc
Yi Li <yil8@uci.edu>
2014-04-09 15:33:12 -0700

    On branch master
    preprocess/io.py added
    preprocess/utils.py added

---
+++ mixclone/preprocess/io.py
@@ -0,0 +1,179 @@
+'''
+Created
+
+@author: Andrew Roth
+
+JointSNVMix-0.6.2
+joint_snv_mix.pre_processing.bam_to_jcnt.BamToJcntConverter
+joint_snv_mix.pre_processing.bam_to_jcnt.JointPileupIterator
+
+================================================================================
+
+Modified on 2013-07-20
+
+@author: Yi Li
+
+pyloh.preprocess.io
+
+================================================================================
+
+Modified on 2014-04-09
+
+@author: Yi Li
+'''
+import sys
+from collections import Counter
+
+import numpy as np
+import pysam
+
+ascii_offset = 33
+
+#JointSNVMix
+class PairedCountsIterator:
+    def __init__(self, paired_pileup_iter, ref_genome_fasta, chrom_name, min_depth=20, min_bqual=10, min_mqual=10):
+        self.paired_pileup_iter = paired_pileup_iter
+        self.ref_genome_fasta = ref_genome_fasta
+        self.chrom_name = chrom_name
+        self.min_depth = min_depth
+        self.min_bqual = min_bqual
+        self.min_mqual = min_mqual
+        
+    def __iter__(self):
+        return self
+    
+    def next(self):
+        normal_column, tumor_column = self.paired_pileup_iter.next()
+        
+        while True:
+            if normal_column.n < self.min_depth or tumor_column.n < self.min_depth:
+                normal_column, tumor_column = self.paired_pileup_iter.next()
+                continue
+            
+            pos = normal_column.pos
+            ref_base = self.ref_genome_fasta.fetch(self.chrom, pos, pos + 1).upper()
+            
+            if ref_base == '':
+                print 'Error: %s does not match the reference of the bam files' \
+                % self.ref_genome_fasta.filename
+                sys.exit(-1)
+            
+            paired_counts = self._get_paired_counts(normal_column, tumor_column, pos, ref_base)
+            
+            if paired_counts == None:
+                normal_column, tumor_column = self.paired_pileup_iter.next()
+                continue
+            else:
+                return paired_counts
+
+    def _get_paired_counts(self, normal_column, tumor_column, pos, ref_base):
+        normal_bases = self._parse_pileup_column(normal_column)
+        tumor_bases = self._parse_pileup_column(tumor_column)
+        
+        normal_non_ref_base, normal_counts = self._get_counts(ref_base, normal_bases) 
+        tumor_non_ref_base, tumor_counts = self._get_counts(ref_base, tumor_bases)        
+
+        # Check again for lines below read depth. The first check above speeds things up, though redundant.
+        normal_depth = normal_counts[0] + normal_counts[1]
+        tumor_depth = tumor_counts[0] + tumor_counts[1]
+    
+        if normal_depth < self.min_depth or tumor_depth < self.min_depth:
+            return None
+    
+        # Shift index to one based position.
+        one_based_pos = pos + 1
+        
+        paired_counts = []
+        paired_counts.extend(normal_counts)
+        paired_counts.extend(tumor_counts)
+        
+        return paired_counts
+               
+    def _parse_pileup_column(self, pileup_column):
+        bases = []
+        
+        for read in pileup_column.pileups:
+            if read.is_del:
+                continue
+            
+            qpos = read.qpos
+                 
+            mqual = read.alignment.mapq
+            
+            if mqual < self.min_mqual:
+                continue            
+            
+            bqual = ord(read.alignment.qual[qpos]) - ascii_offset            
+            
+            if bqual < self.min_bqual:
+                continue
+            
+            base = read.alignment.seq[qpos].upper()
+            bases.append(base)
+        
+        return bases
+
+    def _get_counts(self, ref_base, bases, non_ref_base=None):
+        counter = Counter(bases)
+        
+        non_ref_base, counts = self._parse_counts(ref_base, counter, non_ref_base)
+        
+        return non_ref_base, counts
+    
+    def _parse_counts(self, ref_base, counter, non_ref_base=None):
+        ref_counts = counter[ref_base]
+        
+        del counter[ref_base]
+        del counter['N']
+        
+        # Check if there is any non-ref bases.
+        if non_ref_base is not None:
+            non_ref_counts = counter[non_ref_base]
+        else:
+            if len(counter) > 0:
+                non_ref_base, non_ref_counts = counter.most_common(1)[0]
+            else:
+                non_ref_base = 'N'
+                non_ref_counts = 0
+        
+        counts = (ref_counts, non_ref_counts)
+        
+        return non_ref_base, counts
+
+#JointSNVMix
+class PairedPileupIterator:
+    def __init__(self, normal_iter, tumor_iter, segment_start, segment_end):
+        self.normal_iter = normal_iter
+        self.tumor_iter = tumor_iter
+        self.segment_start = segment_start
+        self.segment_end = segment_end
+    
+    def __iter__(self):
+        return self
+    
+    def next(self):
+        normal_column = self.normal_iter.next()
+        tumor_column = self.tumor_iter.next()
+        
+        while True:                    
+            normal_pos = normal_column.pos
+            tumor_pos = tumor_column.pos
+                  
+            if normal_pos == tumor_pos:
+                if normal_pos >= self.segment_start and normal_pos <= self.segment_end:
+                    return normal_column, tumor_column
+                else:
+                    normal_column = self.normal_iter.next()
+                    tumor_column = self.tumor_iter.next()
+            elif normal_pos < tumor_pos:
+                normal_column = self.normal_iter.next()
+            elif normal_pos > tumor_pos:
+                tumor_column = self.tumor_iter.next()
+            else:
+                raise Exception("Error in paired pileup iterator.")
+
+
+    
+    
+    
+    
---
+++ mixclone/preprocess/utils.py
@@ -0,0 +1,174 @@
+'''
+Created on 2013-07-27
+
+@author: Yi Li
+
+================================================================================
+
+Modified on 2014-04-09
+
+@author: Yi Li
+'''
+import sys
+import numpy as np
+
+from mixclone import constants
+
+def BEDParser(bed_file_name):
+    inbed = open(bed_file_name)
+    
+    chroms = []
+    starts = []
+    ends = []
+    
+    for line in inbed:
+        fields = line.split('\t')
+        chrom_name = fields[0]
+        chrom_ID = chrom_name_to_ID(chrom_name)
+        
+        if chrom_ID == -1:
+            continue
+        
+        chrom_name, start, end = fields[0:3]
+        
+        chroms.append(chrom_name)
+        starts.append(int(start))
+        ends.append(int(end))
+            
+    inbed.close()
+    
+    return (chroms, starts, ends)
+    
+def chrom_ID_to_name(ID, format):
+    if format == 'UCSC':
+        chrom_name = 'chr' + str(ID)
+    elif format == 'ENSEMBL':
+        chrom_name = str(ID)
+    else:
+        print 'Error: %s not supported' % (format)
+        sys.exit(1)
+
+    return chrom_name
+
+def chrom_name_to_ID(chrom_name):
+    ID = -1
+    
+    try:
+        ID = int(chrom_name.strip('chr'))
+    except:
+        pass
+        
+    return ID
+
+def get_chrom_format(chroms):
+    format = 'NONE'
+    
+    for chrom in chroms:
+        if chrom[0:3] == 'chr':
+            format = 'UCSC'
+            break
+        else:
+            try:
+                ID = int(chrom)
+                format = 'ENSEMBL'
+                break
+            except:
+                pass
+    
+    if format == 'NONE':
+        print 'Error: %s not supported' % (chrom)
+        sys.exit(-1)
+    else:
+        return format
+
+def normal_heterozygous_filter(counts):
+    BAF_N_MAX = constants.BAF_N_MAX
+    BAF_N_MIN = constants.BAF_N_MIN
+    
+    I = counts.shape[0]
+    idx_keep = []
+    
+    for i in xrange(0, I):
+        a_N = counts[i, 0]*1.0
+        b_N = counts[i, 1]*1.0
+        d_N = a_N + b_N
+        BAF_N = b_N/d_N
+        
+        if BAF_N >= BAF_N_MIN and BAF_N <= BAF_N_MAX:
+            idx_keep.append(i)
+            
+    counts = counts[idx_keep]
+    
+    return counts
+
+def get_BAF_counts(counts):
+    BAF_bins = constants.BAF_BINS
+    
+    a_N = counts[:, 0]*1.0
+    b_N = counts[:, 1]*1.0
+    a_T = counts[:, 2]*1.0
+    b_T = counts[:, 3]*1.0
+    
+    BAF_N = b_N/(a_N + b_N)
+    BAF_T = b_T/(a_T + b_T)
+    
+    BAF_counts, _, _ = np.histogram2d(BAF_N, BAF_T, bins=(BAF_bins, BAF_bins))
+    
+    return BAF_counts
+
+def tumor_LOH_test(counts, WES_flag):
+    BAF_T_MAX = constants.BAF_T_MAX
+    BAF_T_MIN = constants.BAF_T_MIN
+    LOH_FREC_MAX = constants.LOH_FREC_MAX
+    LOH_FREC_MIN = constants.LOH_FREC_MIN
+    
+    I = counts.shape[0]
+    
+    if WES_flag == True:
+        sites_num_min = constants.SITES_NUM_MIN_WES
+    else:
+        sites_num_min = constants.SITES_NUM_MIN_WGS
+    
+    if I < sites_num_min:
+        LOH_frec = -1
+        LOH_status = 'NONE'
+        
+        return (LOH_frec, LOH_status)
+    
+    LOH_num = 0.0
+    
+    for i in xrange(0, I):
+        a_T = counts[i, 2]*1.0
+        b_T = counts[i, 3]*1.0
+        d_T = a_T + b_T
+        BAF_T = b_T/d_T
+        
+        if BAF_T < BAF_T_MIN or BAF_T > BAF_T_MAX:
+            LOH_num = LOH_num + 1
+    
+    LOH_frec = LOH_num/I
+    
+    if LOH_frec < LOH_FREC_MIN:
+        LOH_status = 'FALSE'
+    elif LOH_frec >= LOH_FREC_MIN and LOH_frec < LOH_FREC_MAX:
+        LOH_status = 'UNCERTAIN'
+    elif LOH_frec >= LOH_FREC_MAX:
+        LOH_status = 'TRUE'
+    else:
+        LOH_status = 'ERROR'
+        
+    return (LOH_frec, LOH_status)
+    
+def remove_outliers(X):
+    idx_keep = []
+    
+    n = X.shape[0]
+    
+    for i in range(0, n):
+        if np.abs(X[i] - X.mean()) <= X.std():
+            idx_keep.append(i)
+            
+    X = X[idx_keep]
+    
+    return X
+    
